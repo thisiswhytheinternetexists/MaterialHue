d74ac7331e22321f0bc218661b0b405cf295ee41
Felix Mann <djflix@gmail.com>
2016-12-18 17:18:37 +0100

    Added Light templates

---
+++ app/src/materialhue/components/details/LightDetails.js
@@ -0,0 +1,10 @@
+import LightDetailsController from './LightDetailsController'
+
+export default {
+  name : 'lightDetails',
+  config : {
+    bindings         : {  selected: '<' },
+    templateUrl      : 'src/materialhue/components/details/LightDetails.html',
+    controller       : [ LightDetailsController ]
+  }
+};
---
+++ app/src/materialhue/components/details/LightDetailsController.js
@@ -0,0 +1,5 @@
+class LightDetailsController  {
+
+}
+export default LightDetailsController;
+
